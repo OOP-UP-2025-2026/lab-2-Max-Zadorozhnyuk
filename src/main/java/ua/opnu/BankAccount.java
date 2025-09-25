e147a7229ab7552d22dc7dc584358ee1838ff101
Maksym Zadorozhniuk <9175593@stud.op.edu.ua>
2025-09-25 21:03:31 +0300

    Lab2 complete

--- src/main/java/ua/opnu/BankAccount.java
+++ src/main/java/ua/opnu/BankAccount.java
@@ -3,11 +3,13 @@ package ua.opnu;
 public class BankAccount {
     String name;
     double balance;
-    double transactionFee;
+    double transactionFee = 0.0;
 
     void deposit(double amount) {
-        // TODO: modify method body
-        balance = balance + amount;
+        if (amount <= 0) {
+            return;
+        }
+        balance += amount;
     }
 
     double getBalance() {
@@ -15,13 +17,27 @@ public class BankAccount {
     }
 
     boolean withdraw(double amount) {
-        // TODO: modify method body
-        balance = balance - amount;
-        return true;
+        if (amount <= 0) {
+            return false;
+        }
+        double total = amount + transactionFee;
+        if (balance >= total) {
+            balance -= total;
+            return true;
+        }
+        return false;
     }
 
     boolean transfer(BankAccount receiver, double amount) {
-        // TODO: modify method body
+        if (amount <= 0) {
+            return false;
+        }
+        double total = amount + transactionFee;
+        if (balance >= total) {
+            balance -= total;
+            receiver.balance += amount;
+            return true;
+        }
         return false;
     }
 }
